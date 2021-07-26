e74a1e4ce5767ed584203b1b0557ac16c8ef4313
jerry_glo <goodEvening@lt.com>
2021-07-26 17:03:21 +0800

    commit second~~~

--- src/main/java/com/lt/bean/Person.java
+++ src/main/java/com/lt/bean/Person.java
@@ -3,5 +3,7 @@ package com.lt.bean;
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
